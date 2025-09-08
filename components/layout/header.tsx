c1d36ae0bc6a9febeffe3c50d6437fde7d821e58
unknown <agusiglesias72@gmail.com>
2025-09-08 14:19:38 -0300

    add

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -245,7 +245,7 @@ export function Header() {
                   </NavigationMenuItem>
 
                   <NavigationMenuItem>
-                    <Link href="/api-docs" legacyBehavior passHref>
+                    <Link href="/documentacion" legacyBehavior passHref>
                       <NavigationMenuLink className="group inline-flex h-10 w-max items-center justify-center rounded-md bg-transparent px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50 cursor-pointer">
                         API Docs
                       </NavigationMenuLink>
